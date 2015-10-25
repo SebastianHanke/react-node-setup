ca0337cba4fa658a343831a9468baac239a520dc
sebastianhanke <sebastian.hanke@yahoo.de>
2015-10-25 15:58:07 +0100

    babel & stylus loaders build & dev

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 var component = require('./component');
 var app = document.createElement('div');
 
+/*
+* import style files into project
+* */
+require('./main.styl');
+
 document.body.appendChild(app);
 
 app.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,78 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var merge = require('webpack-merge');
+var autoPrefixer = require('autoprefixer');
 
-const ROOT_PATH   = path.resolve(__dirname);
-const APP_PATH    = path.resolve(ROOT_PATH, 'app');
-const BUILD_PATH  = path.resolve(ROOT_PATH, 'build');
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH   = path.resolve(__dirname);
+var APP_PATH    = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH  = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
+console.log('app-path...',APP_PATH)
+
+var common = {
     entry   : APP_PATH,
     output  : {
         path    : BUILD_PATH,
         filename: 'bundle.js'
     },
-    devServer: {
-        historyApiFallback  : true,
-        hot                 : true,
-        inline              : true,
-        progress            : true
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test    : /(\.js$)|(\.jsx$)/,
+                include : APP_PATH,
+                loaders : [
+                    'babel-loader'
+                ]
+            },
+            {
+                test    : /\.styl$/,
+                loader  : 'style-loader!css-loader!postcss-loader!stylus-loader',
+                include : APP_PATH
+            }
+        ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             title: 'React on Node'
         })
-    ]
+    ],
+    postcss: [autoPrefixer()]
 };
 
+if(TARGET === 'start') {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback  : true,
+            hot                 : true,
+            inline              : true,
+            progress            : true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+} else if(TARGET === 'build') {
+    module.exports = merge(common, {
+        module: {
+            loaders: [
+                {
+                    test    : /\.styl$/,
+                    loader  : ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!stylus-loader'),
+                    include : APP_PATH
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin("main.css", {
+                allChunks: true
+            })
+        ]
+    });
+}
